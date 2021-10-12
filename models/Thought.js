1700e30b514262f92f1206fa3eb85882fa08e1f0
brandon-stewart-rgb <brandon@pixeltree.us>
2021-10-12 11:52:03 -0600

    Schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,37 +1,73 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: [true, 'Thoughts are required'],
-        minLength: 1,
-        maxLength: 180
-    },
-    createdAt: {
-       type: Date,
-       default: Date.now,
-       timestamps: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    reactions : [{ type: Schema.Types.ObjectId,  ref: 'Reaction'}]
-},
+const ReactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: '',
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			maxLength: 280,
+		},
+		username: {
+			type: String,
+			required: [true, 'username is required'],
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			//    timestamps: true
+			get: (createdAtVal) =>
+				moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+		},
+	},
 
-{
-    toObject: {
-    virtuals: true
-    },
-    toJSON: {
-    virtuals: true 
-    }
-});
+	{
+		toJSON: {
+			getters: true,
+		},
+	}
+);
 
+const ThoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: [true, 'Thoughts are required'],
+			minLength: 1,
+			maxLength: 180,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			//    timestamps: true
+			get: (createdAtVal) =>
+				moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		// reactions : [{ type: Schema.Types.ObjectId,  ref: 'Reaction'}]
+		reactions: [ReactionSchema],
+	},
 
+	{
+		toObject: {
+			virtuals: true,
+		},
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+	}
+);
 
-ThoughtSchema.virtual('reactionCount').get(function(){
-    return this.reactions.length;
+ThoughtSchema.virtual('reactionCount').get(function () {
+	return this.reactions.length;
 });
 
 const Thought = model('Thought', ThoughtSchema);
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const Reaction = require('./Reaction');
+
 const User = require('./User');
 const Thought = require('./Thought');
 
-module.exports = { Reaction, User, Thought };
+module.exports = { User, Thought };
